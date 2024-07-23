645614196daf49f16f3073979f2152bece563cae
shray_gla_git <shray.gupta_cs21@gla.ac.in>
2024-07-23 17:28:59 +0530

    making responsive-2

--- index.js
+++ index.js
@@ -72,19 +72,44 @@ document.getElementById('submit').addEventListener('click', function(e) {
 
 
 
-
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    autoplay: {
+function myFunction(widthSize) {
+    if (widthSize.matches) {
+        // If media query matches
+        const swiper = new Swiper(".swiper", {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            autoplay: {
                 delay: 2500,
                 disableOnInteraction: false,
             },
-  });
+
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    } else {
+        const swiper = new Swiper(".swiper", {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    }
+}
+
+const widthSize = window.matchMedia("(max-width: 780px)");
+// Call listener function at run time
+myFunction(widthSize);
+// Attach listener function on state changes
+widthSize.addListener(myFunction);
+
 
 
 
